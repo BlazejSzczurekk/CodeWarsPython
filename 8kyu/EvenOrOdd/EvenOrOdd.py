543cb482ff8eebdecdb07bf335807ca8e1e58414
BlazejSzczurekk <blazej.szczurek26@gmail.com>
2025-05-23 12:35:07 +0200

    Check if the number is Odd/Even

---
+++ 8kyu/EvenOrOdd/EvenOrOdd.py
@@ -0,0 +1,11 @@
+#Solution:
+def EvenOrOdd(number):
+    if number%2==0:
+        return "Even"
+    else:
+        return "Odd"
+    
+#Test:
+print(EvenOrOdd(1))
+print(EvenOrOdd(241))
+print(EvenOrOdd(36))
